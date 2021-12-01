2a3b1a8c84653a1031227a9f353b485dc72d49ed
Dr. Raimund K. Ege <ege@niu.edu>
2021-12-01 15:36:42 -0600

    logic is now complete

--- components/Credits.js
+++ components/Credits.js
@@ -3,16 +3,36 @@ app.component('credits', {
         /*html*/
         `<div class="display"> 
             <div class="item-right">
-                <button @click="getCredits()">Update</button>
+                <button @click="current=null;getCredits()">Update</button>
             </div>
             <h3>List of recent credit transactions:</h3>
-            <div v-for="cr in data">
-                {{ cr }}
+            <div class="displaygrid">
+                <div>
+                    <div v-for="cr in data" class="creditgrid credit" v-on:click="current=cr" :class="{ 'text-red': cr==current }">
+                        <div>{{ formatRelative(cr.timeStamp) }}</div>
+                        <div>{{ formatSome(cr.name) }}</div>
+                        <div>\${{ cr.amount }}</div>
+                    </div>               
+                </div>           
+                <div v-if="current" class="detail">
+                <strong>Transaction Detail:</strong><br>
+                    <div v-for="key in Object.keys(current)">
+                        <div v-if="key!=='_id'">                     
+                            <div v-if="key=='timeStamp'" class="detailgrid">
+                                <div>{{ key }}:</div><div>{{ formatTime(current[key]) }}</div>
+                            </div>
+                            <div v-else class="detailgrid">
+                                <div>{{ key }}:</div><div>{{ current[key] }}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
          </div>`,
     data() {
         return {
-            credits: null
+            credits: null,
+            current: null
         }
     },
     methods: {
@@ -20,21 +40,41 @@ app.component('credits', {
             NProgress.start()
             var resp = await axios.get('../CreditCard/')
             this.credits = resp.data         
-            console.log(this.credits)
+            // console.log(this.credits)
             NProgress.done()
         },
-        formatDate(when) {
-            var d = new Date(when).toISOString();
-            return d.slice(0, 10) + ' ' + d.slice(11, 16)
+        formatRelative(when) {
+            var d = new Date() - new Date(when);
+            var seconds = Math.round(d/1000)
+            var minutes = Math.round(d/1000/60)
+            var hours = Math.round(d/1000/60/60)
+            // console.log(seconds, minutes, hours);
+            if (seconds<60) {
+                return seconds.toString() + " second" + ((seconds==1)?'':'s') + " ago"
+            } else if (minutes<60) {
+                return minutes.toString() + " minute" + ((minutes==1)?'':'s') + " ago"
+            } else if (hours<6) {
+                return hours.toString() + " hour" + ((hours==1)?'':'s') + " ago"
+            } else { 
+                return this.formatTime(when)
+            }
+        },
+        formatSome(data) {          
+            return (data.length > 20)?(data.slice(0,19) + "..."):data
+        },
+        formatTime(when) {
+            var d = new Date(when);
+            var text = d.getFullYear() + "-" + (d.getMonth()+1).toString().padStart(2,'0') + "-" + d.getDate().toString().padStart(2,'0') + " ";
+            text += d.getHours().toString().padStart(2,'0') + ":" + d.getMinutes().toString().padStart(2,'0');           
+            return text;
         }
     },
     computed: {
-        data() {
-            var data = this.credits;            
+        data() {           
             if (this.credits == null ) {
                 this.getCredits()
             }            
-            return data
+            return this.credits
         }
     }
 })
--- components/Orders.js
+++ components/Orders.js
@@ -3,16 +3,36 @@ app.component('orders', {
         /*html*/
         `<div class="display"> 
             <div class="item-right">
-                <button @click="getOrders()">Update</button>
+                <button @click="current=null;getOrders()">Update</button>
             </div>
             <h3>List of recent purchase orders:</h3>
-            <div v-for="po in data">
-                {{ formatDate(po.timeStamp) }} {{ po.custid }} {{ po.order }}
+            <div class="displaygrid">
+                <div>
+                    <div v-for="po in data" class="ordergrid order" v-on:click="current=po" :class="{ 'text-red': po==current }">
+                        <div>{{ formatRelative(po.timeStamp) }}</div>
+                        <div>{{ formatSome(po.name) }}</div>
+                        <div>\${{ po.amount }}</div>
+                    </div>
+                </div>           
+                <div v-if="current" class="detail">
+                    <strong>Order Detail:</strong><br>
+                    <div v-for="key in Object.keys(current)">
+                        <div v-if="key!=='_id'">                     
+                            <div v-if="key=='timeStamp'" class="detailgrid">
+                                <div>{{ key }}:</div><div>{{ formatTime(current[key]) }}</div>
+                            </div>
+                            <div v-else class="detailgrid">
+                                <div>{{ key }}:</div><div>{{ current[key] }}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
          </div>`,
     data() {
         return {
-            orders: null
+            orders: null,
+            current: null
         }
     },
     methods: {
@@ -20,21 +40,41 @@ app.component('orders', {
             NProgress.start()
             var resp = await axios.get('../PurchaseOrder/')
             this.orders = resp.data         
-            console.log(this.orders)
+            // console.log(this.orders)
             NProgress.done()
         },
-        formatDate(when) {
-            var d = new Date(when).toISOString();
-            return d.slice(0, 10) + ' ' + d.slice(11, 16)
+        formatRelative(when) {
+            var d = new Date() - new Date(when);
+            var seconds = Math.round(d/1000)
+            var minutes = Math.round(d/1000/60)
+            var hours = Math.round(d/1000/60/60)
+            // console.log(seconds, minutes, hours);
+            if (seconds<60) {
+                return seconds.toString() + " second" + ((seconds==1)?'':'s') + " ago"
+            } else if (minutes<60) {
+                return minutes.toString() + " minute" + ((minutes==1)?'':'s') + " ago"
+            } else if (hours<6) {
+                return hours.toString() + " hour" + ((hours==1)?'':'s') + " ago"
+            } else { 
+                return this.formatTime(when)
+            }
+        },
+        formatSome(data) {          
+            return (data.length > 25)?(data.slice(0,24) + "..."):data
+        },
+        formatTime(when) {
+            var d = new Date(when);
+            var text = d.getFullYear() + "-" + (d.getMonth()+1).toString().padStart(2,'0') + "-" + d.getDate().toString().padStart(2,'0') + " ";
+            text += d.getHours().toString().padStart(2,'0') + ":" + d.getMinutes().toString().padStart(2,'0');           
+            return text;
         }
     },
     computed: {
-        data() {
-            var data = this.orders;            
+        data() {            
             if (this.orders == null ) {
                 this.getOrders()
             }            
-            return data
+            return this.orders
         }
     }
 })
