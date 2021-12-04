133d01bd834acb04526149dd237217fc10379ce5
RaimundEge <ege@niu.edu>
2021-12-04 13:15:43 -0800

    updated scrolling behavior

--- components/Credits.js
+++ components/Credits.js
@@ -3,31 +3,30 @@ app.component('credits', {
         /*html*/
         `<div class="display"> 
             <div class="item-right">
+                <span v-if="!current">
+                    click on a credit transaction to see detail &nbsp;
+                </span>
                 <button @click="current=null;getCredits()">Update</button>
             </div>
-            <h3>List of recent credit transactions:</h3>
-            <div class="displaygrid">
-                <div>
-                    <div v-for="cr in data" class="creditgrid credit" v-on:click="current=cr" :class="{ 'text-red': cr==current }">
-                        <div>{{ formatRelative(cr.timeStamp) }}</div>
-                        <div>{{ formatSome(cr.name) }}</div>
-                        <div>\${{ cr.amount }}</div>
-                    </div>               
-                </div>           
-                <div v-if="current" class="detail">
-                <strong>Transaction Detail:</strong><br>
-                    <div v-for="key in Object.keys(current)">
-                        <div v-if="key!=='_id'">                     
-                            <div v-if="key=='timeStamp'" class="detailgrid">
-                                <div>{{ key }}:</div><div>{{ formatTime(current[key]) }}</div>
-                            </div>
-                            <div v-else class="detailgrid">
-                                <div>{{ key }}:</div><div>{{ current[key] }}</div>
-                            </div>
+            <h3>List of recent credit transactions:</h3>          
+            <div v-for="cr in data" class="creditgrid credit" v-on:click="current=cr" :class="{ 'text-red': cr==current }">
+                <div>{{ formatRelative(cr.timeStamp) }}</div>
+                <div>{{ formatSome(cr.name) }}</div>
+                <div>\${{ cr.amount }}</div>
+            </div>                                        
+            <div v-if="current" class="detail">
+            <strong>Transaction Detail:</strong><br>
+                <div v-for="key in Object.keys(current)">
+                    <div v-if="key!=='_id'">                     
+                        <div v-if="key=='timeStamp'" class="detailgrid">
+                            <div>{{ key }}:</div><div>{{ formatTime(current[key]) }}</div>
+                        </div>
+                        <div v-else class="detailgrid">
+                            <div>{{ key }}:</div><div>{{ current[key] }}</div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>           
          </div>`,
     data() {
         return {
@@ -39,16 +38,16 @@ app.component('credits', {
         async getCredits() {
             NProgress.start()
             var resp = await axios.get('../CreditCard/')
-            this.credits = resp.data         
+            this.credits = resp.data
             // console.log(this.credits)
             NProgress.done()
         }
     },
     computed: {
-        data() {           
-            if (this.credits == null ) {
+        data() {
+            if (this.credits == null) {
                 this.getCredits()
-            }            
+            }
             return this.credits
         }
     }
--- components/Orders.js
+++ components/Orders.js
@@ -3,31 +3,30 @@ app.component('orders', {
         /*html*/
         `<div class="display"> 
             <div class="item-right">
+                <span v-if="!current">
+                    click on a purchase order to see detail &nbsp;
+                </span>
                 <button @click="current=null;getOrders()">Update</button>
             </div>
-            <h3>List of recent purchase orders:</h3>
-            <div class="displaygrid">
-                <div>
-                    <div v-for="po in data" class="ordergrid order" v-on:click="current=po" :class="{ 'text-red': po==current }">
-                        <div>{{ formatRelative(po.timeStamp) }}</div>
-                        <div>{{ formatSome(po.name) }}</div>
-                        <div>\${{ po.amount }}</div>
-                    </div>
-                </div>           
-                <div v-if="current" class="detail">
-                    <strong>Order Detail:</strong><br>
-                    <div v-for="key in Object.keys(current)">
-                        <div v-if="key!=='_id'">                     
-                            <div v-if="key=='timeStamp'" class="detailgrid">
-                                <div>{{ key }}:</div><div>{{ formatTime(current[key]) }}</div>
-                            </div>
-                            <div v-else class="detailgrid">
-                                <div>{{ key }}:</div><div>{{ current[key] }}</div>
-                            </div>
+            <h3>List of recent purchase orders:</h3>            
+            <div v-for="po in data" class="ordergrid order" v-on:click="current=po" :class="{ 'text-red': po==current }">
+                <div>{{ formatRelative(po.timeStamp) }}</div>
+                <div>{{ formatSome(po.name) }}</div>
+                <div>\${{ po.amount }}</div>
+            </div>                          
+            <div v-if="current" class="detail">
+                <strong>Order Detail:</strong><br>
+                <div v-for="key in Object.keys(current)">
+                    <div v-if="key!=='_id'">                     
+                        <div v-if="key=='timeStamp'" class="detailgrid">
+                            <div>{{ key }}:</div><div>{{ formatTime(current[key]) }}</div>
+                        </div>
+                        <div v-else class="detailgrid">
+                            <div>{{ key }}:</div><div>{{ current[key] }}</div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>          
          </div>`,
     data() {
         return {
